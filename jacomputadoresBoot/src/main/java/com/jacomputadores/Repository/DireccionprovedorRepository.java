dbcebcd1fac70d195aa94ae668b3388b79b384ae
roa2013 <115506830+roa2013@users.noreply.github.com>
2023-10-20 13:19:18 -0500

    Add files via upload

---
+++ jacomputadoresBoot/src/main/java/com/jacomputadores/Repository/DireccionprovedorRepository.java
@@ -0,0 +1,7 @@
+package com.jacomputadores.Repository;
+
+import com.jacomputadores.Entity.DireccionProvedor;
+import org.springframework.data.repository.CrudRepository;
+
+public interface DireccionprovedorRepository  extends CrudRepository<DireccionProvedor , Integer> {
+}
